457cb74b36e3cf8f01dd6ab24b5316e36af7e3bd
Benjamin Lowe <benjamin@sc0ville.com>
2020-10-07 22:35:02 +0900

    add initial files

---
+++ mymodulename/mode2/bar.py
@@ -0,0 +1,5 @@
+from mymodulename.mode1.do_wonders import foo
+
+def boom():
+    print("Calling bar.py's boom() which is importing from mode1.do_wonders!")
+    foo()
---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Optionally install ThriAnalysis as a module"""
+
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name="mymodulename",
+                 version="X",
+                 description="",
+                 author="Scoville",
+                 python_requires=">=2.7",
+                 packages=setuptools.find_packages(),
+                )
